9ca2c78a81362bf415dfd32086ecaf56f27acea3
14796682966@163.com <Zyx/2019>
2020-07-20 23:33:03 +0800

    SpringBoot的四种属性注入

--- Readme.md
+++ Readme.md
@@ -2,6 +2,21 @@
 
 ### SpringBoot补充学习
 
+- 注解
+   - @Controller/@RestController 控制器/rest控制器
+   - @EnableAutoConfiguration
+   - @ComponentScan
+   - @SpringApplication 启动器
+   - @SpringBootConfiguration
+   - @Configuration
+   - @PropertiesSource("classpath:")
+   - @Bean
+   - @Value("${}")
+   - @ConfigurationProperties(prefix = "jdbc")
+   - @@EnableConfigurationProperties(JdbcProperties.class)
+   
+
+
 - 异常处理
     - Spring注入Druid配置类:无法创建bean的异常
         - 原因：是缺少jdbc相关的jar包， 
@@ -13,6 +28,32 @@
                   两个文件都有效，但是application.properties的优先级会比application.yml高。
         -  配置文件所在目录不同优先级也不同。如下图1~4优先级从高到低:
         - ![图片](https://img-blog.csdn.net/20180921103443224?watermark/2/text/aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L3UwMTQwNDk1MjE=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70)
-
-       
-                
+    - 读取配置文件的四种方式
+        - @PropertySource("classpath:jdbc.properties") //读取Property配置文件,@Value("${}")注入睡醒
+        - @ConfigurationProperties(prefix = "jdbc") //直接通过在方法上添加注释读取配置属性
+            ```java
+             @ConfigurationProperties(prefix = "jdbc")
+                public DataSource dataSource() {
+                    DruidDataSource dataSource = new DruidDataSource();
+                    return dataSource;
+                }
+            ```   
+        - 创建配置类,@EnableConfigurationProperties(JdbcProperties.class)读取,可复用
+            ```java
+            @Data
+            @Component //交由IOC容器管理,使用@Configuration也一样
+            @ConfigurationProperties(prefix = "jdbc")
+            public class JdbcProperties {
+                private String url;
+                private String driverClassName;
+                private String username;
+                private String password;
+            }
+            ```     
+        - 上面的基础上直接传入形参即可:public DataSource dataSource(JdbcProperties jdbcProperties) {}
+    - SpringBoot的四种属性注入
+        - @Autowired/@Resource
+        - 构造方法注入
+        - @Bean方法形参注入
+        - 直接在@Bean方法上使用@ConfigurationProperties(prefix = "jdbc")注解
+            
--- SpringBoot_Start/src/main/java/org/zyx/config/JdbcConfiguration.java
+++ SpringBoot_Start/src/main/java/org/zyx/config/JdbcConfiguration.java
@@ -1,24 +1,24 @@
 package org.zyx.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import org.springframework.boot.autoconfigure.jdbc.JdbcProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 /**
- * JDBC 数据源,连接池配置类(基于Spring)
+ * JDBC 数据源,连接池配置类
  */
 @Configuration //声明这个类是一个java配置类,相当于一个xml配置文件
 //@PropertySource("classpath:jdbc.properties") //读取Property配置文件
+//3.通过属性读取类获取配置,自动注入,允许自动读取配置属性
 @EnableConfigurationProperties(JdbcProperties.class)
 public class JdbcConfiguration {
 
     /**
-     * 通过读取Property配置文件设置属性
+     * 1.通过读取Property配置文件设置属性
      * @return
      */
 //    @Value(value = "${jdbc.url}")
@@ -30,12 +30,12 @@ public class JdbcConfiguration {
 //    @Value(value = "${jdbc.password}")
 //    private String password;
 
-    @Resource
-    private org.zyx.config.JdbcProperties jdbcProperties;
+//    @Resource //自动注入
+//    private org.zyx.config.JdbcProperties jdbcProperties;
 
 
     @Bean //把方法的返回值注入到IOC容器中
-    public DataSource dataSource() {
+    public DataSource dataSource(JdbcProperties jdbcProperties) {//2.通过形参注入配置bean
         DruidDataSource dataSource = new DruidDataSource();
         //添加this,区分局部和全局
 //        dataSource.setUrl(this.url);
@@ -43,9 +43,8 @@ public class JdbcConfiguration {
 //        dataSource.setPassword(this.password);
 //        dataSource.setDriverClassName(this.driverClassName);
 
-        /**
-         * 通过配置读取类获取参数
-         */
+
+        //通过配置读取类获取参数
         dataSource.setUrl(jdbcProperties.getUrl());
         dataSource.setUsername(jdbcProperties.getUsername());
         dataSource.setPassword(jdbcProperties.getPassword());
@@ -53,6 +52,15 @@ public class JdbcConfiguration {
 
         return dataSource;
     }
+
+    //4.直接通过在方法上添加注释读取配置属性
+/*
+    @ConfigurationProperties(prefix = "jdbc")
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
+*/
 
 
 }
